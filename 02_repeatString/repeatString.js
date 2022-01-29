3fcc6dcdeddf5249f1c9b362242d7d529a84b571
Olguta Fratila <fratilaolguta150@gmail.com>
2022-01-29 18:10:11 +0200

    In progress

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, time) {
+    let repeatedString = '';
+    if(time > 0) {
+        repeatedString = string.repeat(time);
+    } else if(time === 0) {
+        repeatedString = '';
+    } else {
+        repeatedString = 'ERROR';
+    }
+    return repeatedString;
+}
 
-};
+repeatString('hey', -1);
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const splitWord = string.split("");
+    const reverseArray = splitWord.reverse();
+    const joinArray = reverseArray.join("");
 
+    return joinArray;
 };
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
