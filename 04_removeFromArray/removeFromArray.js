a07c52a62f569bcc6d2ff4d3a63332edc457037b
Olguta Fratila <fratilaolguta150@gmail.com>
2022-01-30 23:23:24 +0200

    In progress

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+// const removeFromArray = function(array, ...toRemove) {
+//     let modifiedArray = [...array];
+//     for(i = 0; i < array.length; i++) {
+//         if(modifiedArray.includes(toRemove[i])) {
+//             modifiedArray.splice(modifiedArray.indexOf(toRemove[i]), 1);
+//         }
+//     }
+//     return modifiedArray;
+// }
 
-};
+
+const removeFromArray = function(array, ...toRemove) {
+    let modifyArray = [...array];
+    for(i = 0; i < array.length; i++){
+        if(modifiedArray)
+    }
+}
+
+removeFromArray([1, 2, 3, 4], 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
